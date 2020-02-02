3a712c93ada18d90f54a2876add4250e260f9eed
prashob <itsprashob@gmail.com>
2020-02-02 17:42:24 +0530

    Header updated

---
+++ src/common/header/Header.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import './Header.css';
+class Header extends Component{
+     
+     
+    render(){
+        return(
+            <div className="app-header">Image Viewer</div>
+        )
+    }
+}   
+
+export default Header;
